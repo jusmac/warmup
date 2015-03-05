b3f21891cbdf799431e09b67436edb8c2f4ecb6b
jusmac <justyna.machalica@gmail.com>
2015-03-05 17:59:18 +0100

    stylowanie wtyczek i animacji

---
+++ words.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template Name: words* */
+
+$context = Timber::get_context();
+$post = Timber::query_post();
+$context['post'] = $post;
+$context['wp_title'] .= ' - ' . $post->title();
+$context['comment_form'] = TimberHelper::get_comment_form();
+
+if (post_password_required($post->ID)) {
+    Timber::render('single-password.twig', $context);
+} else {
+    Timber::render(array('single-' . $post->ID . '.twig', 'single-' . $post->post_type . '.twig', 'words.twig'), $context);
+}
